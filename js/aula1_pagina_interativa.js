2ff3e2be017baac1e776aeafc8cc265a495c0eeb
Lucas Silva <lucasilvabr@gmail.com>
2024-11-23 17:00:13 -0300

    update files
    Finalizado aula 1

--- js/aula1_pagina_interativa.js
+++ js/aula1_pagina_interativa.js
@@ -3,6 +3,8 @@ let count = 0;
 const cores = ['#007bff', '#28a745', '#dc3545', '#ffc107', '#17a2b8'];
 let coresIndex = 0;
 
+const ideias = ['Jogar Video Game', 'Jantar Fora', 'Ir a Praia', 'Rolê de Moto', 'Pedir Ifood'];
+
 function comecaContagem(){
     count++;
     document.getElementById('contador').textContent = count;
@@ -15,5 +17,10 @@ function resetaContagem(){
 
 function mudaCor(){
     coresIndex = (coresIndex + 1) % cores.length;
-    document.getElementById('caixaColorida').style.backgroundColor = cores[coresIndex]
+    document.getElementById('caixaColorida').style.backgroundColor = cores[coresIndex];
+}
+
+function geraIdeias(){
+    const ideiasIndex = Math.floor(Math.random() * ideias.length);
+    document.getElementById('ideiaRandom').textContent = ideias[ideiasIndex];
 }
